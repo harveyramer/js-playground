48743d7f5d6fc5d6080c427cb9eb52ac823a36bb
Harvey Ramer <harvey@harveyramer.com>
2022-08-07 18:07:09 -0400

    Simplify by moving map/sort to helper functions.

--- sorting/complex-sort.js
+++ sorting/complex-sort.js
@@ -28,27 +28,27 @@ const formatDays = (days) => {
         return dayMap[k];
     });
 };
+const formatClass = (c) => {
+    c.meeting_days = formatDays(c.meeting_days);
+    c.meeting_days = c.meeting_days.sort(sortDays);
+    return {
+        name: c.name,
+        meeting_days: c.meeting_days,
+        formatted_meeting_days: c.meeting_days.join(', '),
+        reg_start_date: c.reg_start_date,
+        start_date: c.start_date,
+        end_date: c.end_date,
+    };
+};
+const sortClasses = (a, b) => {
+    let day1 = a.meeting_days[0].toLowerCase();
+    let day2 = b.meeting_days[0].toLowerCase();
+    const dateCompare = Date.parse(b.reg_start_date) - Date.parse(a.reg_start_date);
+    const daysCompare = dayDictionary[day1] - dayDictionary[day2];
+    return dateCompare || daysCompare;
+};
 export const fetchData = async () => {
     const result = await fetch('https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsJson?OrgID=524744&Cat2=Private&Session=22/23%20School%20Year%20(Sept-June)');
     const json = await result.json();
-    const formatted = json.rows.map((c) => {
-        c.meeting_days = formatDays(c.meeting_days);
-        c.meeting_days = c.meeting_days.sort(sortDays);
-        return {
-            name: c.name,
-            meeting_days: c.meeting_days,
-            formatted_meeting_days: c.meeting_days.join(', '),
-            reg_start_date: c.reg_start_date,
-            start_date: c.start_date,
-            end_date: c.end_date,
-        };
-    });
-    return formatted
-        .sort((a, b) => {
-        let day1 = a.meeting_days[0].toLowerCase();
-        let day2 = b.meeting_days[0].toLowerCase();
-        const dateCompare = Date.parse(b.reg_start_date) - Date.parse(a.reg_start_date);
-        const daysCompare = dayDictionary[day1] - dayDictionary[day2];
-        return dateCompare || daysCompare;
-    });
+    return json.rows.map(formatClass).sort(sortClasses);
 };
--- sorting/complex-sort.ts
+++ sorting/complex-sort.ts
@@ -24,36 +24,34 @@ const sortDays = (a: string, b: string) => {
   let day1 = a.toLowerCase();
   let day2 = b.toLowerCase();
   return dayDictionary[day1] - dayDictionary[day2];
-}
+};
 const formatDays = (days: any) => {
   return Object.keys(days).filter((k: string) => days[k]).map((k: string) => {
     return dayMap[k];
   });
-}
+};
+const formatClass = (c: any) => {
+  c.meeting_days = formatDays(c.meeting_days);
+  c.meeting_days = c.meeting_days.sort(sortDays);
+  return {
+    name: c.name,
+    meeting_days: c.meeting_days,
+    formatted_meeting_days: c.meeting_days.join(', '),
+    reg_start_date: c.reg_start_date,
+    start_date: c.start_date,
+    end_date: c.end_date,
+  };
+};
+const sortClasses = (a: any, b: any) => {
+  let day1 = a.meeting_days[0].toLowerCase();
+  let day2 = b.meeting_days[0].toLowerCase();
+  const dateCompare = Date.parse(b.reg_start_date) - Date.parse(a.reg_start_date);
+  const daysCompare = dayDictionary[day1] - dayDictionary[day2];
+  return dateCompare || daysCompare;
+};
 
 export const fetchData = async () => {
   const result = await fetch('https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsJson?OrgID=524744&Cat2=Private&Session=22/23%20School%20Year%20(Sept-June)');
   const json: any = await result.json();
-  // const json = test;
-  const formatted = json.rows.map((c: any) => {
-    c.meeting_days = formatDays(c.meeting_days);
-    c.meeting_days = c.meeting_days.sort(sortDays);
-    return {
-      name: c.name,
-      meeting_days: c.meeting_days,
-      formatted_meeting_days: c.meeting_days.join(', '),
-      reg_start_date: c.reg_start_date,
-      start_date: c.start_date,
-      end_date: c.end_date,
-    };
-  });
-  return formatted
-    .sort((a: any, b: any) => {
-      let day1 = a.meeting_days[0].toLowerCase();
-      let day2 = b.meeting_days[0].toLowerCase();
-      const dateCompare = Date.parse(b.reg_start_date) - Date.parse(a.reg_start_date);
-      const daysCompare = dayDictionary[day1] - dayDictionary[day2];
-      // console.log(dateCompare, daysCompare);
-      return dateCompare || daysCompare;
-    });
+  return json.rows.map(formatClass).sort(sortClasses);
 }
